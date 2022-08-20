4a748adec2198ccd633e133db74f96c6ff2ddc28
Daniel Dias Pereira <86617987+DanielDPereira@users.noreply.github.com>
2022-08-19 21:42:13 -0300

    Update estatisticas.py

--- estatisticas.py
+++ estatisticas.py
@@ -1,16 +1,23 @@
-#
+likes_input = input('Insira os likes dos posts (separe com uma ","): ')
 
-númerodelikes = []
+# Transformando os likes em um array de string (que vai ser convertido pra número depois)
+likes_list_string = likes_input.split(',')
 
-print("Insira os likes de cada post, colocando uma , depois de cada elemento ")
-likes = input().split(",")
+# Passando por cada elemento da lista, transformando em números e adicioando
+# à uma nova lista que só vai ter números
+likes_list = []
 
-likes = int(likes)
-númerodelikes.extend(likes)
-númerodelikes = str(númerodelikes)
+for elemento in likes_list_string:
+    likes_convertido_em_numero = int(elemento)
 
-print("Média de likes por post = ")
+    # adicionando o numero de likes do post no array
+    likes_list.append(likes_convertido_em_numero) 
+    
+# Estatísticas
+total_likes = sum(likes_list)
+total_posts = len(likes_list) # Tamanho da lista (quantos elementos tem) é o número de posts
+average_likes_per_post = total_likes / total_posts
 
-print(sum(númerodelikes) / len(númerodelikes))
-
-print("Quantidade de posts = ", len(númerodelikes))
+print('Total de Likes:', total_likes)
+print('Total de Posts:', total_posts)
+print('Média de Likes por Posts:', average_likes_per_post)
