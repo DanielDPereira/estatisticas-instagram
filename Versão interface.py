2f9fc75e27dfbd63c34b29967d22b9f4b0a2ac7f
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-01-17 10:08:52 -0300

    Update Versão interface.py

--- Versão interface.py
+++ Versão interface.py
@@ -25,7 +25,11 @@ while True:
     seguidores = int(values[1])
     likes_input = values[2]
     
+    # Transformando os likes em um array de string (que vai ser convertido pra número depois)
     likes_list_string = likes_input.split(',')
+    
+    # Passando por cada elemento da lista, transformando em números e adicioando
+    # à uma nova lista que só vai ter números
     likes_list = []
     
     for elemento in likes_list_string:
@@ -33,6 +37,11 @@ while True:
 
         # adicionando o numero de likes do post no array
         likes_list.append(likes_convertido_em_numero) 
+        
+    # Estatísticas
+    total_likes = sum(likes_list)
+    total_posts = len(likes_list) # Tamanho da lista (quantos elementos tem) é o número de posts
+    average_likes_per_post = total_likes / total_posts
     
     print(nome_relatorio)
     print(seguidores)
